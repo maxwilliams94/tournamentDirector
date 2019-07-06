688c8b5c62fdf9bb6c40eacaf3388d571e281631
Max Williams <me@maxwillia.ms>
2019-07-06 22:55:08 +0000

    Add tournament timings (hardcoded) into nested class Timings

--- tournament.cs
+++ tournament.cs
@@ -7,6 +7,7 @@ namespace tournament
     {
         public Tournament(string name, string[] team_list)
         {
+
             int totalTeams = team_list.Length;
             // Create Hashtable of team objects with key as seed, value a Team instance
             Hashtable teams = new Hashtable();
@@ -16,7 +17,21 @@ namespace tournament
                 teams.Add(i, aTeam);
                 Console.WriteLine(aTeam);
             }
+            Console.WriteLine($"Read in {0} teams", totalTeams);
 
+
+
+        }
+        // Timings
+        class Timings
+        {
+            public Timings()
+            {
+                DateTime startDateTime = new DateTime(2019, 1, 1, 9, 0, 0);
+                DateTime EndDateTime = new DateTime(2019, 1, 2, 17, 0, 0);
+                int gameTimeInMinutes = 55;
+                int breakTimeinMinutes = 5;
+            }
         }
     }
 
